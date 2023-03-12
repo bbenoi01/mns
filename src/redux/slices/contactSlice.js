d72a561265733c226ef39f6c39ee250d82af2646
bbenoi01 <b.nutty@gmail.com>
2023-03-11 20:10:12 -0800

    removed email route from server

--- src/redux/slices/contactSlice.js
+++ src/redux/slices/contactSlice.js
@@ -9,7 +9,7 @@ export const sendEmail = createAsyncThunk(
 	'contact/send_email',
 	async (data, { rejectWithValue }) => {
 		try {
-			const res = await axios.post('/email', data);
+			const res = await axios.post('http://localhost:3005/email', data);
 			return res.data;
 		} catch (err) {
 			return rejectWithValue(err.response.data);
