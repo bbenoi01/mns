32c44233aa94ca8ccb92b5c4d353bff0fdfe27cc
bbenoi01 <b.nutty@gmail.com>
2023-05-01 17:15:04 -0700

    turned hero into video

--- src/features/home/components/hero/Hero.jsx
+++ src/features/home/components/hero/Hero.jsx
@@ -2,9 +2,24 @@ import './hero.scss';
 
 const Hero = () => {
 	return (
-		<>
-			<div id='home-hero'></div>
-			<div className='hero-text'>
+		// <>
+		// 	<div id='home-hero'></div>
+		// 	<div className='hero-text'>
+		// 		<h3>Inspiring new love connections.</h3>
+		// 		<h5>~ I am a ~</h5>
+		// 		<div>
+		// 			<a className='link visitor-type' href='#clients'>
+		// 				Client
+		// 			</a>
+		// 			<a className='link visitor-type' href='#candidates'>
+		// 				Candidate
+		// 			</a>
+		// 		</div>
+		// 	</div>
+		// </>
+		<div className='hero'>
+			<video src='nanny.mp4' muted autoPlay loop />
+			<div className='hero-txt'>
 				<h3>Inspiring new love connections.</h3>
 				<h5>~ I am a ~</h5>
 				<div>
@@ -16,7 +31,7 @@ const Hero = () => {
 					</a>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
